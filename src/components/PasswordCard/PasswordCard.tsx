2709c6c1b9f67ba5f087501118cec831d13c7798
Marco Mereu <thayoungcruel@gmail.com>
2026-08-02 16:09:08 +0200

    feat(ui): add password preview section

--- src/components/PasswordCard/PasswordCard.tsx
+++ src/components/PasswordCard/PasswordCard.tsx
@@ -1,11 +1,22 @@
 import styles from "./PasswordCard.module.css";
+import { Copy } from "lucide-react";
 
 function PasswordCard() {
   return (
-    <section className={styles.card}>
-      <p>Password will appear here</p>
-    </section>
-  );
+  <section className={styles.card}>
+    <div className={styles.passwordHeader}>
+      <span className={styles.password}>
+        hG8$L!x2Qm@9Pw#K
+      </span>
+
+      <button className={styles.copyButton}>
+        <Copy size={18} />
+
+        <span>Copy</span>
+      </button>
+    </div>
+  </section>
+);
 }
 
 export default PasswordCard;
